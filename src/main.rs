adf039abc1d820b6d2b8917182ba292c2612af11
ihab65 <ihg.bk65@gmail.com>
2023-09-10 17:38:05 -0400

    refactor + improvements

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,61 @@
 use std::env;
 
-const CURRENCIES: [&str; 2] = ["usd", "eur"];
-
-fn print_as_word(number: String) {
-    let mut output_str = String::from("-> ");
-    match number.len() {
-
-            num if num == 4 => {
-                for (i, c) in number.chars().enumerate() {
-                    if i != 3 {    
-                        output_str.push(c);
-                    }
-                }
-                output_str.push_str(" alf")
-            },
-
-            num if num == 5 => {
-                for (i, c) in number.chars().enumerate() {
-                    if i == 0 && c == '1'{
-                        output_str.push_str("million o ")
-                    } else if i == 0 {
-                        output_str.push_str(format!("{} millions o ", c).as_str())
-                    } else if i != 0 && i != 4 {
-                        output_str.push(c)
-                    }
-                }
-            },
-
-            num if num == 6 => {
-                
-                for (i, c) in number.chars().enumerate() {
-                    if i == 0 {
-                        output_str.push(c)
-                    } else if i == 1 {
-                        output_str.push(c);
-                        output_str.push_str(" millions o ")
-                    } else if i!= 0 && i != 1 && i != 5 {
-                        output_str.push(c)
-                    }
-                }
-            },
-
-            num if num == 7 => {
-                
-                for (i, c) in number.chars().enumerate() {
-                    if i == 0 {
-                        output_str.push(c)
-                    } else if i == 1 {
-                        output_str.push(c)
-                    } else if i == 2 {
-                        output_str.push(c);
-                        output_str.push_str(" millions o ")
-                    } else if i!= 0 && i != 1 && i != 2 && i != 6 {
-                        output_str.push(c)
-                    }
-                }
-            },
-
-            num if num == 8 => {
-                for (i, c) in number.chars().enumerate() {
-                    if i == 0 && c == '1' {
-                        output_str.push_str(" milliard o ")
-                    } else if i == 0 && c != '1' {
-                        output_str.push(c);
-                        output_str.push_str(" milliards o ")
-                    } else if i == 1 {
-                        output_str.push(c)
-                    } else if i == 2 {
-                        output_str.push(c)
-                    } else if i == 3 {
-                        output_str.push(c);
-                        output_str.push_str(" millions")
-                    }
-                }
-            }
-
-            _ => (),
-
-        } // end of match
-        if output_str.ends_with("000") {
-            output_str = output_str.trim_end_matches("o 000").to_string();
-        } else if output_str.ends_with("o 000 millions") {
-            output_str = output_str.trim_end_matches("o 000 millions").to_string()
-        } else if output_str.ends_with("-> ") {
-            output_str = output_str.trim_end_matches("-> ").to_string();
-        }
-        println!("{}", output_str)
-
-    }
-    
-
-fn format_num(cstr: String) -> String {
-    cstr
-        .as_bytes()
-        .rchunks(3)
-        .rev()
-        .map(std::str::from_utf8)
-        .collect::<Result<Vec<&str>, _>>()
-        .unwrap()
-        .join(",")
+#[derive(PartialEq)]
+struct Currency<'a> {
+    name: &'a str,
+    value: f32,
 }
 
-fn calculate(sum: f64 , unit: &str) {
-
-    if CURRENCIES.contains(&unit) {
-        let output = sum*220f64;
-        let cstr = format!("{}", output.round());
-        println!("");
-        print!("{} {} is {} dzd ", sum, unit, format_num(cstr.clone()));
-        print_as_word(cstr)
-
-    } else if unit == "dzd" {
-        let output = sum/220f64;
-        let cstr = format!("{}", output.round());
-        println!("");
-        println!("{} {} is {} usd or eur", sum, unit, format_num(cstr))
+impl<'a> Currency<'a> {
+    const fn new(code: &'a str, price: f32) -> Self {
+        Currency { name: code, value: price }
     }
 }
+
+const CURRENCIES: [Currency; 3] = [
+    Currency::new("USD", 210f32),
+    Currency::new("EUR", 220f32),
+    Currency::new("GBP", 250f32)
+];
 
 
 fn main() {
-    let mut args = env::args();
-    args.next().unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    let sum = args
-        .next()
-        .unwrap()
-        .parse::<f64>()
-        .expect("ERROR: couldn't convert");
-
-    let binding = args
-        .next()
+    let sum = args[1]
+        .parse::<f32>()
+        .map_err(|err| eprintln!("ERROR: failed parsing the sum argument: {err}"))
         .unwrap();
-    let unit = binding.as_str();
 
-    calculate(sum, unit)
+    let unit = &args[2];
+    let currency = CURRENCIES.iter().find(|&c| c.name == unit.to_uppercase()).unwrap();
+
+    as_word(calculate(sum, currency) as u32)
+}
+
+fn calculate(sum: f32, unit: &Currency<'_>) -> f32 {
+    sum * unit.value
+}
+
+fn as_word(mut amount: u32) {
+    let b = amount / 10_000_000;
+    amount = amount % 10_000_000;
+    let m = amount / 10_000;
+    amount = amount % 10_000;
+    let k = (amount / 1_000) * 100;
+    let c = amount % 1_000;
+
+    if b != 0 {
+        print!(" {}mlyar", b);
+    }
+    if m != 0 {
+        print!(" {}mlyoun", m);
+    }
+    if k != 0 {
+        print!(" {}alf", k);
+    }
+    if c != 0 {
+        print!(" {}", c);
+    }
+    println!(" dzd");
 }
